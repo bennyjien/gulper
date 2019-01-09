af41445fe289b5edbdb635177241c140b78bc13d
Benny Jien <bennyjien@Bens-MacBook-Pro.local>
2019-01-09 10:52:38 +0700

    01.09

--- gulpfile.js
+++ gulpfile.js
@@ -46,7 +46,7 @@ gulp.task('kit-js-dist', function() {
 		.pipe(kit())
 		.pipe(prettify({indent_char: '\t', indent_size: 1, preserve_newlines: true, unformatted: ['a', 'span', 'img', 'code', 'pre', 'sub', 'sup', 'em', 'strong', 'b', 'i', 'u', 'strike', 'big', 'small', 'pre', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'svg','br', 'label', 'input', 'script', 'time'], wrap_line_length: 0}))
 		.pipe(useref({ searchPath: './' }))
-		.pipe(gulpIf('assets/js/bundle.js', babel({ presets: ['es2015'] })))
+		.pipe(gulpIf('assets/js/bundle.js', babel({ presets: ['env'] })))
 		.pipe(gulpIf('assets/js/bundle.js', uglify()))
 		.pipe(gulp.dest('dist/'));
 });
