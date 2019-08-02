ce3047295d83f526f9c493438641be7fcb935b07
Benny Jien <bennyjien@Bens-MacBook-Pro.local>
2019-08-02 13:32:43 +0700

    Initial dist subtree commit

---
+++ dist/assets/js/script.js
@@ -0,0 +1,13 @@
+/* This file contains main script for website
+ * Style related scripts is located in style.js
+ */
+/* global document Flickity equalheight scrollFunction fluidvids */
+
+(function() {
+	// fluidvids
+	fluidvids.init({
+		selector: ['.js-video'],
+		players: ['www.youtube.com']
+	});
+
+})();
