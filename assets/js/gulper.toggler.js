e8a6d46f86c39c0711f7dcb83ed7125af9daca35
Benny Jien <benny@jien.me>
2020-01-02 18:19:47 +0700

    Update gulper.toggler.js

--- assets/js/gulper.toggler.js
+++ assets/js/gulper.toggler.js
@@ -1,6 +1,7 @@
 // toggler.js: toggle element state
 // TODO: pass options object on callback
 // TODO: by default toggle direct child
+// TODO: can still be interated with after ajax
 /* OPTIONS
 	data-toggle-trigger="click|hover" -> how will toggle be triggered
 	data-toggle-target="[selector]" -> toggle target
